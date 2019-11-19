0aa383db9cd4efb8a6d88a6ea2412d2a536808b8
Maria Lapshina <lapmarser@gmail.com>
2019-11-18 19:01:34 -0500

    models small commit

---
+++ backend/db/models/Question.js
@@ -0,0 +1,24 @@
+const connection = require('../connection')
+const { Sequelize } = connection;
+const {UUID, UUIDV4, TEXT, STRING} = Sequelize
+
+module.exports = connection.define('question,'{
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  text: {
+    type: TEXT
+  },
+  testCaseId:{
+    
+  },
+  functionName: {
+    type:STING 
+  },
+  parameters: {
+    type:
+  }
+
+})
