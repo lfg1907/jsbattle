35bc95383f360cfe74a6e6f820dd66a7a3d6a70f
Jesen Tanadi <mail@jesentanadi.com>
2019-11-21 07:01:22 -0500

    add css to Editor

--- frontend/src/components/Editor/index.jsx
+++ frontend/src/components/Editor/index.jsx
@@ -1,15 +1,19 @@
+// Simple wrapper around the CodeMirror component
+// so we don't have to deal with boilerplate stuff all the time
 import React from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/theme/neat.css';
+import './style.css';
 import 'codemirror/mode/javascript/javascript';
 
-const Editor = () => {
+const Editor = ({ value }) => {
   return (
     <CodeMirror
-      value=""
+      value={value}
+      className="goose-teeth"
       options={{
         mode: 'javascript',
         theme: 'material',
