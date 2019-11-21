33d957034502a5ba4a608bd546aa4647889a1537
Jesen Tanadi <mail@jesentanadi.com>
2019-11-21 05:40:35 -0500

    lint and clean up

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
 
-render(<Provider store={store}><App/></Provider>, document.querySelector('#root'));
+import store from './store';
+import App from './components/App';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
