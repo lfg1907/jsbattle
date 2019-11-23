588918ba1eedf9b9ef24f9c5b4d79b50361e6bd6
Jesen Tanadi <mail@jesentanadi.com>
2019-11-23 15:46:43 -0500

    add games route

---
+++ backend/routes/games.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const { Game } = require('../db/models');
+
+const router = express.Router();
+router.use(express.json());
+
+// GET /api/games
+router
+  .route('/')
+  .get((req, res, next) => {
+    Game.findAll()
+      .then(games => res.send(games))
+      .catch(next);
+  })
+  .post((req, res, next) => {
+    const { gameData } = req.body;
+    Game.create(gameData)
+      .then(game => res.status(201).send(game))
+      .catch(next);
+  });
+
+// GET, PUT, DELETE
+// /api/games/:id
+router
+  .route('/:id')
+  .get((req, res, next) => {
+    Game.findByPk(req.params.id)
+      .then(game => res.send(game))
+      .catch(next);
+  })
+  .put((req, res, next) => {
+    Game.findByPk(req.params.id)
+      .then(game => game.update(req.body))
+      .catch(next);
+  })
+  .delete((req, res, next) => {
+    Game.findByPk(req.params.id)
+      .then(game => game.destroy)
+      .then(() => res.sendStatus(204))
+      .catch(next);
+  });
+
+module.exports = router;
