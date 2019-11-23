58bf3753747f8a2053f3317d80d844d7ea8a5a5c
Jesen Tanadi <mail@jesentanadi.com>
2019-11-23 14:54:55 -0500

    add other models

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,7 +1,13 @@
+const Game = require('./Game');
 const Question = require('./Question');
 const TestCase = require('./TestCase');
+const Player = require('./Player');
+const User = require('./User');
 
 module.exports = {
+  Game,
   Question,
-  TestCase
+  TestCase,
+  Player,
+  User
 };
