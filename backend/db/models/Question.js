ca17ad5742b6942462a6041f993cd1a990ab3581
Jesen Tanadi <mail@jesentanadi.com>
2019-11-23 14:52:48 -0500

    add properties, add static method

--- backend/db/models/Question.js
+++ backend/db/models/Question.js
@@ -3,18 +3,22 @@ const connection = require('../connection');
 const { Sequelize } = connection;
 const { UUID, UUIDV4, TEXT, STRING } = Sequelize;
 
-module.exports = connection.define('question', {
+const Question = connection.define('question', {
   id: {
     type: UUID,
     primaryKey: true,
     defaultValue: UUIDV4
   },
-  text: {
+  prompt: {
     type: TEXT,
     allowNull: false,
     validate: {
       notEmpty: true
     }
+  },
+  examples: {
+    type: TEXT,
+    allowNull: false
   },
   functionName: {
     type: STRING,
@@ -31,3 +35,15 @@ module.exports = connection.define('question', {
     }
   }
 });
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+Question.findRandom = async () => {
+  const questions = await this.findAll();
+  const randomIdx = getRandom(0, questions.length);
+  return questions[randomIdx];
+};
+
+module.exports = Question;
