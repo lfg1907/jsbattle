b1524c89b8d5b6a5dbe2542462b03edd4181eafe
Jesen Tanadi <mail@jesentanadi.com>
2019-11-23 16:03:44 -0500

    add method to find winner instead of storing id in database

--- backend/db/models/Game.js
+++ backend/db/models/Game.js
@@ -3,6 +3,8 @@ const connection = require('../connection');
 const { Sequelize } = connection;
 const { BOOLEAN, INTEGER, UUID, UUIDV4 } = Sequelize;
 
+const Player = require('./Player');
+
 const Game = connection.define('game', {
   id: {
     type: UUID,
@@ -17,11 +19,21 @@ const Game = connection.define('game', {
   completed: {
     type: BOOLEAN,
     defaultValue: false
-  },
-  winnerId: {
-    type: UUID,
-    allowNull: true
   }
 });
+
+Game.prototype.findWinningPlayer = function findWinningPlayer() {
+  if (!this.completed) return {};
+
+  return Player.findAll({
+    where: { gameId: this.id }
+  }).then(players => {
+    return players.reduce((winner, player) => {
+      if (!winner.score || player.score > winner.score)
+        return player;
+      return winner;
+    }, {});
+  });
+};
 
 module.exports = Game;
