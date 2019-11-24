da4fe6b08c006d3938ab284e8738e94fc1e44ff7
Jesen Tanadi <mail@jesentanadi.com>
2019-11-23 22:05:49 -0500

    mark game as finished when all game questions have been answered

--- backend/db/models/GameQuestion.js
+++ backend/db/models/GameQuestion.js
@@ -1,26 +1,48 @@
 const connection = require('../connection');
+const Game = require('./Game');
 
 const { Sequelize } = connection;
 const { BOOLEAN, UUID, UUIDV4 } = Sequelize;
 
-const GameQuestion = connection.define('gameQuestion', {
-  id: {
-    type: UUID,
-    primaryKey: true,
-    defaultValue: UUIDV4
+const GameQuestion = connection.define(
+  'gameQuestion',
+  {
+    id: {
+      type: UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4
+    },
+    completed: {
+      type: BOOLEAN,
+      defaultValue: false
+    },
+    gameId: {
+      type: UUID,
+      allowNull: false
+    },
+    questionId: {
+      type: UUID,
+      allowNull: false
+    }
   },
-  completed: {
-    type: BOOLEAN,
-    defaultValue: false
-  },
-  gameId: {
-    type: UUID,
-    allowNull: false
-  },
-  questionId: {
-    type: UUID,
-    allowNull: false
+  {
+    hooks: {
+      afterUpdate(gameQuestion) {
+        GameQuestion.updateGame(gameQuestion.gameId);
+      }
+    }
   }
-});
+);
+
+GameQuestion.updateGame = async function(gameId) {
+  const incompleteGameQuestions = this.findAll({
+    where: { gameId, completed: false }
+  });
+  const game = await Game.findByPk(gameId);
+
+  if (!incompleteGameQuestions.length) {
+    await game.update({ inProgress: false });
+  }
+};
 
 module.exports = GameQuestion;
