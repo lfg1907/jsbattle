53d4b5bd2236adb1a8b6091008c9f80c56e95da9
Jesen Tanadi <mail@jesentanadi.com>
2019-11-24 22:28:03 -0500

    add gamesReducer

--- frontend/src/store/reducer.js
+++ frontend/src/store/reducer.js
@@ -1,11 +1,25 @@
 import { combineReducers } from 'redux';
 
+import { FETCH_GAMES, CREATE_GAME } from './constants';
+
 const questionReducer = (state = []) => {
   return state;
 };
 
+const gamesReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_GAMES:
+      return action.games;
+    case CREATE_GAME:
+      return [action.game, ...state];
+    default:
+      return state;
+  }
+};
+
 const reducer = combineReducers({
-  questions: questionReducer
+  questions: questionReducer,
+  games: gamesReducer
 });
 
 export default reducer;
