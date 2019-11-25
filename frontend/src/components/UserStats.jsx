219fb60483e32d4ecdc2ae4768a93e32ca7073c6
Jesen Tanadi <mail@jesentanadi.com>
2019-11-24 22:29:27 -0500

    initial commit, new components

---
+++ frontend/src/components/UserStats.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserStats = () => {
+  return (
+    <div id="user-stats-container">
+      <div id="user-stats">
+        <h2>Hello User</h2>
+        <p>You&apos;ve won xx games out of xx.</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserStats;
