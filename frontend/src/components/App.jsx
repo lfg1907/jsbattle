0fa0480ea056e0dd052a1525e18dc9c2c15281bf
Jesen Tanadi <mail@jesentanadi.com>
2019-11-24 22:27:29 -0500

    add routes to App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,12 +3,16 @@ import { connect } from 'react-redux';
 import { HashRouter, Route } from 'react-router-dom';
 
 // Components
+import Home from './Home';
+import WaitingRoom from './WaitingRoom';
 import EditorView from './EditorView';
 
 const App = () => {
   return (
     <HashRouter>
+      <Route path="/home" component={Home} />
       <Route path="/editor" component={EditorView} />
+      <Route path="/waiting/:id" component={WaitingRoom} />
     </HashRouter>
   );
 };
