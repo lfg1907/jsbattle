8838f3682515197e2281091b2f1779361b5c80cf
Jesen Tanadi <mail@jesentanadi.com>
2019-12-01 15:56:46 -0500

    use websocket to update game status

--- frontend/src/components/GameToJoinItem.jsx
+++ frontend/src/components/GameToJoinItem.jsx
@@ -1,24 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
+import socket from '../socket';
+
 const GameToJoinItem = ({ game, handleSelect }) => {
+  const [currentGame, setCurrentGame] = useState(game);
+  socket.on('game joined', gameData => {
+    if (gameData.game.id === currentGame.id) {
+      setCurrentGame(gameData.game);
+    }
+  });
+
   const [status, setStatus] = useState('');
   useEffect(() => {
-    if (game.status === 'IN_PROGRESS') {
-      setStatus('In progress');
-    } else if (game.status === 'COMPLETED') {
+    if (currentGame.status === 'IN_PROGRESS') {
+      setStatus('In Progress');
+    } else if (currentGame.status === 'COMPLETED') {
       setStatus('Completed');
-    } else if (game.numOfPlayers >= 3) {
+    } else if (currentGame.numOfPlayers >= 3) {
       setStatus('Full');
+    } else {
+      setStatus(`${currentGame.numOfPlayers}/3`);
     }
-  }, []);
+  }, [currentGame]);
 
   return (
     // eslint-disable-next-line
     <div
       className={
-        game.status === 'STARTING' && game.numOfPlayers < 3
-          ? 'game'
-          : 'game inactive-game'
+        status === 'In Progress' ||
+        status === 'Completed' ||
+        status === 'Full'
+          ? 'game inactive-game'
+          : 'game'
       }
       key={game.id}
       role="radio"
