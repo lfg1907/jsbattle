266fb74f2757b7707a1fafeae5ace48bda37614d
Jesen Tanadi <mail@jesentanadi.com>
2019-12-05 00:01:17 -0500

    move question to separate component; load functionName in editor

--- frontend/src/components/EditorView.jsx
+++ frontend/src/components/EditorView.jsx
@@ -5,64 +5,32 @@ import { actions } from '../store';
 import Editor from './Editor';
 
 const EditorPage = ({
-  questions,
-  testCases,
+  currentQ,
   testResults,
-  getAllQuestions,
-  getTestCases,
   getTestResults
 }) => {
-  const [editorValue, setEditorValue] = useState('');
-  const [testCasesLoaded, setTestCasesLoaded] = useState(
-    false
+  const funcString = `function ${currentQ.question.functionName}(${currentQ.question.params}) {\n  \n}`;
+  const [editorValue, setEditorValue] = useState(
+    funcString
   );
   const handleEditorChange = ev => {
     setEditorValue(ev.doc.getValue());
   };
-
-  useEffect(() => {
-    getAllQuestions();
-  }, []);
-
-  if (!questions.length) {
-    return '...loading';
-  }
-
-  if (!testCasesLoaded) {
-    getTestCases(questions[0].id);
-    setTestCasesLoaded(true);
-  }
 
   const countWrongResults = results => {
     return results.filter(result => !!result.wrong).length;
   };
 
   const testCode = () => {
-    getTestResults(questions[0].id, editorValue);
+    getTestResults(currentQ.questionId, editorValue);
   };
-
-  // always shows first question for now
-  // when all test cases are in, show random question
 
   return (
     <div>
-      <h3>{questions[0].prompt}</h3>
       <Editor
         value={editorValue}
         onChange={handleEditorChange}
       />
-      <div id="test-case-div">
-        <h4>Test Cases:</h4>
-        <ul>
-          {testCases.map(testCase => (
-            <li key={testCase.id}>
-              {`${testCase.arguments} `}
-              should yield
-              {` ${testCase.answer}`}
-            </li>
-          ))}
-        </ul>
-      </div>
       <div id="console-logs">
         {testResults.length &&
         testResults[0].consoles.length ? (
@@ -106,26 +74,16 @@ const EditorPage = ({
   );
 };
 
-const mapStateToProps = ({
-  questions,
-  testCases,
-  testResults
-}) => {
+const mapStateToProps = ({ testResults }) => {
   return {
-    questions,
-    testCases,
     testResults
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    getTestCases: qID =>
-      dispatch(actions.fetchTestCases(qID)),
     getTestResults: (qID, code) =>
-      dispatch(actions.fetchTestResults(qID, code)),
-    getAllQuestions: () =>
-      dispatch(actions.getAllQuestions())
+      dispatch(actions.fetchTestResults(qID, code))
   };
 };
 
