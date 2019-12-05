2027531685c9cbe9e6c51b501b7b98e44cc8b21e
Jesen Tanadi <mail@jesentanadi.com>
2019-12-05 00:02:40 -0500

    use GameView to contain question and editor, load questions

--- frontend/src/components/GameView.jsx
+++ frontend/src/components/GameView.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
-const GameView = ({ game, questions }) => {
+import { actions } from '../store';
+
+import EditorView from './EditorView';
+import QuestionView from './QuestionView';
+
+const GameView = ({
+  game,
+  gameQuestions,
+  currentQ,
+  getGameQuestions
+}) => {
+  useEffect(() => {
+    getGameQuestions(game.id);
+  }, []);
+
+  if (!gameQuestions.length) {
+    return '...loading';
+  }
+
+  if (!currentQ) {
+    return 'game is done';
+  }
+
   return (
-    <div>
+    <div id="game-view">
       <h2>{`Game ${game.name}`}</h2>
+      <div id="game-container">
+        <QuestionView currentQ={currentQ} />
+        <EditorView currentQ={currentQ} />
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (
-  { games, questions },
+  { games, gameQuestions },
   { match: { params } }
 ) => ({
   game: games.find(_game => _game.id === params.id),
-  questions
+  gameQuestions,
+  // return the first incomplete question
+  currentQ: gameQuestions.find(gameQ => !gameQ.completed)
 });
 
-export default connect(mapStateToProps)(GameView);
+const mapDispatchToProps = dispatch => ({
+  getGameQuestions(gameId) {
+    dispatch(actions.getGameQuestions(gameId));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameView);
