55259836e9ca6d97f5330a9ccad4a29d910d9d22
Jesen Tanadi <mail@jesentanadi.com>
2019-12-05 21:54:57 -0500

    button completes question, function name in Editor is updated

--- frontend/src/components/EditorView.jsx
+++ frontend/src/components/EditorView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { actions } from '../store';
 
@@ -8,12 +8,16 @@ import EditorOutputs from './EditorOutputs';
 const EditorPage = ({
   currentQ,
   testResults,
-  getTestResults
+  getTestResults,
+  completeQuestion
 }) => {
-  const funcString = `function ${currentQ.question.functionName}(${currentQ.question.params}) {\n  // Your code here...\n}`;
-  const [editorValue, setEditorValue] = useState(
-    funcString
-  );
+  const [editorValue, setEditorValue] = useState('');
+  useEffect(() => {
+    const funcString = `function ${currentQ.question.functionName}(${currentQ.question.params}) {
+  // Your code here...\n}`;
+    setEditorValue(funcString);
+  }, [currentQ.id]);
+
   const handleEditorChange = ev => {
     setEditorValue(ev.doc.getValue());
   };
@@ -23,7 +27,7 @@ const EditorPage = ({
   };
 
   const submitCode = () => {
-    return null;
+    completeQuestion(currentQ.id);
   };
 
   return (
@@ -52,7 +56,9 @@ const mapStateToProps = ({ testResults }) => {
 const mapDispatchToProps = dispatch => {
   return {
     getTestResults: (qID, code) =>
-      dispatch(actions.fetchTestResults(qID, code))
+      dispatch(actions.fetchTestResults(qID, code)),
+    completeQuestion: qID =>
+      dispatch(actions.completeQuestion(qID))
   };
 };
 
