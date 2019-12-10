6c39e24d99cc133af4f8030d26edcf93bf6cee3c
Jesen Tanadi <mail@jesentanadi.com>
2019-12-10 08:18:02 -0500

    update username styling

--- frontend/src/components/UserStats.jsx
+++ frontend/src/components/UserStats.jsx
@@ -4,7 +4,10 @@ const UserStats = ({ name, gamesWon, gamesPlayed }) => {
   return (
     <div id="user-stats-container">
       <div id="user-stats">
-        <h1>{`Hello ${name}`}</h1>
+        <h1>
+          <span className="light">Hello </span>
+          {name}
+        </h1>
         <p>
           {`You've won ${gamesWon} games out of ${gamesPlayed} games played.`}
         </p>
